2fc24d96bf8d172d7611a7e21a236d1325e2adaa
Claudio Araujo <calloman123@gmail.com>
2023-12-26 16:55:25 -0600

    fixed neotree

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -5,7 +5,7 @@ return {
       { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
       { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
     },
-    onts = {
+    opts = {
       options = {
         mode = "tabs",
         show_buffer_close_icons = false,
