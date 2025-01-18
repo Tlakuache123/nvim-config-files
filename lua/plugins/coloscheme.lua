8f1adc71fa86ffce01bddfc18b18206d23d4c14e
Claudio Araujo <77213711+Tlakuache123@users.noreply.github.com>
2025-01-18 00:58:39 -0600

    style: custom colors for neotree and Snacks

--- lua/plugins/coloscheme.lua
+++ lua/plugins/coloscheme.lua
@@ -19,7 +19,29 @@ return {
   },
   {
     "neanias/everforest-nvim",
+    lazy = true,
     name = "everforest",
+    opts = {
+      on_highlights = function(hl, palette)
+        -- VIM
+        hl.Normal = { fg = palette.fg, bg = palette.bg_dim }
+        hl.NormalNC = { fg = palette.fg, bg = palette.bg_dim }
+        hl.WinSeparator = { fg = palette.bg0, bg = palette.bg_dim }
+
+        -- NeoTree
+        hl.NeoTreeFileName = { fg = palette.fg, bg = palette.none }
+        hl.NeoTreeDirectoryName = { fg = palette.fg, bg = palette.none }
+        hl.NeoTreeGitAdded = { fg = palette.green, bg = palette.none }
+        hl.NeoTreeGitModified = { fg = palette.blue, bg = palette.none }
+        hl.NeoTreeGitUnstaged = { fg = palette.orange, bg = palette.none }
+        hl.NeoTreeGitUntracked = { fg = palette.aqua, bg = palette.none }
+
+        -- Snacks
+        hl.SnacksNormal = { fg = palette.fg, bg = palette.bg_dim }
+        hl.SnacksNormalNC = { fg = palette.fg, bg = palette.bg_dim }
+        hl.SnacksWinBarNC = { fg = palette.aqua, bg = palette.none }
+      end,
+    },
   },
   {
     "LazyVim/LazyVim",
